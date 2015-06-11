f43752aa65fd8685216e0a4c0c60a61aeb79d623
tangkikodo <allmonday@126.com>
2015-06-11 11:43:45 +0800

    up

--- scripts/cp.sh
+++ scripts/cp.sh
@@ -2,9 +2,18 @@
 # function: clear folder and paste new stuff from target 
 # assign your source fold and target folder here
 source="/Users/tangmin/tour-of-linux/test-folders/copy-test/source/"
-
 dest="/Users/tangmin/tour-of-linux/test-folders/copy-test/dest/"
 
-cd $dest && rm -rf `ls -Ab`
-echo "${PWD##*/}"
-cp -a $source $dest 
+echo "please check your ?[Yn]"
+read input
+echo $input
+
+if [ "$input" == "Y" ]; then 
+    cd $dest && rm -rf `ls -Ab`
+    echo "${PWD##*/}"
+    cp -a $source $dest 
+    echo "success"
+else
+    echo "abort"
+fi
+
